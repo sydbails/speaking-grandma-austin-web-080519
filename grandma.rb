a9b1018032622701f80eaaa204c929e634407b7f
Syd Bailey <sydabailey@gmail.com>
2019-07-25 15:48:58 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,12 +1,9 @@
 def speak_to_grandma(phrase)
-  if
-    phrase == phrase.downcase
+  if phrase == phrase.downcase
       return "HUH?! SPEAK UP, SONNY!"
-  elsif
-    phrase == phrase.upcase
+  elsif phrase == phrase.upcase
       return "NO, NOT SINCE 1938!"
-  elseif
-    phrase == "I LOVE YOU GRANDMA!"
+  elsif phrase == "I LOVE YOU GRANDMA!"
       return "I LOVE YOU TOO PUMPKIN!"
 end
 
