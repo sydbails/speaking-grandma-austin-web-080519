42d2c2d22e65383470056b174d6ec98205f623b7
Syd Bailey <sydabailey@gmail.com>
2019-07-25 15:43:21 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,8 @@
 def speak_to_grandma ("phrase")
-  if 
+  if
     ${phrase} == speak_to_grandma.downcase
       return "HUH?! SPEAK UP, SONNY!"
-  elsif 
+  elsif
     ${phrase} == speak_to_grandma.uppercase
       return "NO, NOT SINCE 1938!"
   elseif
@@ -15,7 +15,7 @@ def speak_to_grandma ("phrase")
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
